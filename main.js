3d47c1fdf9290c2a56180c8cd9f4b2074468d422
shuangdatdang <108745634+shuangdatdang@users.noreply.github.com>
2022-07-18 09:54:46 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,64 @@
+// Baby Names Data (Top 50 Boy/Girl 2022)
+// Baby Center (babycenter.com)
+// https://www.babycenter.com/baby-names/most-popular/top-baby-names#popularNameList
+
+// Variables for html elements
+let goBtn = document.getElementById("go-btn");
+let menuSelect = document.getElementById("menu-select");
+let container = document.getElementById("container");
+let nameCountSpan = document.getElementById("name-count");
+
+// Initialize Array of Character Objects from json file
+let babyData = [];
+fetch("baby-names-data.json")
+  .then((res) => res.json())
+  .then((data) => (babyData = data));
+
+// Event Listener on Go Button
+goBtn.addEventListener("click", goBtnClicked);
+
+// Process Go Button Click
+function goBtnClicked() {
+  // Get Menu Selection
+  let selection = menuSelect.value;
+
+  // Process Menu Selection
+  if (selection === "display-all") {
+    displayAll();
+  } else if (selection === "gender") {
+    searchGender();
+  } else if (selection === "rank") {
+    searchRank();
+  } else if (selection === "starting-letter") {
+    searchStartingLetter();
+  } else if (selection === "length") {
+    searchLength();
+  }
+}
+
+// Display All Baby Names
+function displayAll() {
+  console.log("Display All");
+  // Confirm data load
+  console.log(babyData);
+}
+
+// Display Names by Gender
+function searchGender() {
+  console.log("Search By Gender");
+}
+
+// Display Names within a Range of Ranks
+function searchRank() {
+  console.log("Search By Rank");
+}
+
+// Display Names with Starting Letter
+function searchStartingLetter() {
+  console.log("Search by Starting Letter");
+}
+
+// Display Names with a Specific Length
+function searchLength() {
+  console.log("Search by Name Length");
+}
